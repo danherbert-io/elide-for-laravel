b36bec63fbdcfe7a4001cf7fdd8418aedcbdd99e
Dan Herbert <daniel.eherbert@gmail.com>
2025-07-11 10:14:23 +0930

    * facade generator command

--- src/Htmx.php
+++ src/Htmx.php
@@ -4,19 +4,17 @@ declare(strict_types=1);
 
 namespace Elide;
 
-use Elide\Http\Response;
-use Elide\View\Partial;
 use Illuminate\Support\Facades\Facade;
-use Illuminate\View\Component;
-use Illuminate\View\View;
 
 /**
- * @method static Services\Htmx rootView(string $view)
- * @method static Services\Htmx usingPartials(callable $callable)
- * @method static Response render(Partial|View|Component|string $component, array $props = [])
- * @method static Partial partial(View|Component|string $component, array $props = [])
- * @method static Services\Htmx sendWithResponse(Partial|View|Component|string $partial)
- * @method static Partial[] flushPendingPartials()
+ * @method static \Elide\Services\Htmx rootView(string $view)
+ * @method static \Elide\Services\Htmx usingPartials(callable $callable, \Elide\Enums\RequestKind $for = 'both')
+ * @method static \Elide\Http\Response render(\Elide\View\Partial|\Illuminate\View\View|\Illuminate\View\Component|string $component, array $props = [], string|null $partialName = 'content')
+ * @method static \Elide\Services\Htmx sendWithResponse(\Elide\View\Partial|\Illuminate\View\View|\Illuminate\View\Component|string $partial)
+ * @method static \Elide\View\Partial partial(\Illuminate\View\View|\Illuminate\View\Component|string $component, array $props = [], string|null $name = null)
+ * @method static array flushPendingPartials()
+ *
+ * @see \Elide\Services\Htmx
  */
 class Htmx extends Facade
 {
